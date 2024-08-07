6a8efa17eb2a6f4b176cd340271c8963eb7951fa
AJAYKRISHNAN S <sjajaykrishna0808@gmail.com>
2024-08-07 10:48:30 +0530

    first commit

---
+++ src/components/CarouseSlide.js
@@ -0,0 +1,23 @@
+import Carousel from 'react-bootstrap/Carousel';
+
+function UncontrolledExample() {
+  return (
+    <div>
+        <div>
+        <Carousel>
+      <Carousel.Item style={{height:"300px"}}>
+        
+      </Carousel.Item>
+      <Carousel.Item style={{height:"300px"}}>
+     
+      </Carousel.Item>
+      <Carousel.Item  style={{height:"300px"}}>
+       
+      </Carousel.Item>
+    </Carousel>
+    </div>
+    </div>
+  );
+}
+
+export default UncontrolledExample;
